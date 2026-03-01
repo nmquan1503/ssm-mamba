d7822ef9191dd083b100a909a7e06809b04f2c98
nmquan1503 <minhquan15032005@gmail.com>
2026-03-01 18:02:21 +0700

    feat: add Python bindings for selective_scan

---
+++ csrc/bindings.cpp
@@ -0,0 +1,8 @@
+#include <torch/extension.h>
+
+#include "selective_scan/selective_scan.h"
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("forward", &selective_scan_forward, "Selective scan forward");
+    m.def("backward", &selective_scan_backward, "Selective scan backward");
+}
