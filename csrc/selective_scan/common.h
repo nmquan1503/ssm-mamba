fd8671564846985fa6168ec0b179f7a7d61be2db
nmquan1503 <minhquan15032005@gmail.com>
2026-03-01 22:54:52 +0700

    feat: add block-level load/store utilities for global memory

--- csrc/selective_scan/common.h
+++ csrc/selective_scan/common.h
@@ -13,3 +13,50 @@ constexpr size_t max_of(std::initializer_list<size_t> list) {
     }
     return max_val;
 }
+
+template<typename Traits>
+inline __device__
+void load(
+    float* src, 
+    float (&dst)[Traits::kNumElements], 
+    typename Traits::ScalarBlockLoad::TempStorage& smem,
+    int num_valid_elements
+) {
+    if constexpr (Traits::kSeqDivisible) {
+        auto& smem_vec = reinterpret_cast<typename Traits::VectorBlockLoad::TempStorage&>(smem);
+        typename Traits::VectorBlockLoad(smem_vec).Load(
+            reinterpret_cast<float4*>(src),
+            reinterpret_cast<float4(&)[Traits::kNumVectors]>(dst)
+        );
+    }
+    else {
+        typename Traits::ScalarBlockLoad(smem).Load(src, dst, num_valid_elements 0.f);
+    }
+}
+
+template<typename Traits>
+inline __device__
+void store(
+    const float (&src)[Traits::kNumElements],
+    float* dst,
+    typename Traits::ScalarBlockStore::TempStorage& smem,
+    int num_valid_elements
+) {
+    float vals[Traits::kNumElements];
+
+    #pragma unroll
+    for (int i = 0; i < Traits::kNumElements; i++) {
+        vals[i] = src[i];
+    }
+    
+    if constexpr (Traits::kSeqDivisible) {
+        auto& smem_vec = reinterpret_cast<typename Traits::VectorBlockStore::TempStorage&>(smem);
+        typename Traits::VectorBlockStore(smem_vec).Store(
+            reinterpret_cast<float4*>(dst),
+            reinterpret_cast<float4(&)[Traits::kNumVectors]>(vals)
+        );
+    }
+    else {
+        typename Traits::ScalarBlockStore(smem).Store(dst, src, num_valid_elements);
+    }
+}
