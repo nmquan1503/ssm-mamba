7f987a154c2ce58249c7cb3faaf2134b8eab3584
nmquan1503 <minhquan15032005@gmail.com>
2026-03-04 16:47:47 +0700

    feat: implement Mamba block with caching for inference

---
+++ modules/mamba.py
@@ -0,0 +1,225 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from typing import Literal
+
+from ..ops import SelectiveScanFn, SelectiveUpdateFn
+
+class Mamba(nn.Module):
+    def __init__(
+        self,
+        model_dim: int = 512,
+        state_dim: int = 16,
+        conv_kernel: int = 4,
+        expansion_factor: int = 2,
+        delta_rank: int | str = "auto",
+        delta_min: float = 0.001,
+        delta_max: float = 0.1,
+        delta_init: Literal["random", "constant"] = "random",
+        delta_scale: float = 1.0,
+        delta_init_floor: float = 1e-4,
+        conv_bias: bool = False,
+        bias: bool = False,
+        device: str | None = None
+    ):
+        super().__init__()
+
+        self.model_dim = model_dim
+        self.state_dim = state_dim
+        self.inner_dim = expansion_factor * model_dim
+        self.conv_kernel = conv_kernel
+        self.delta_rank = math.ceil(model_dim / 16) if delta_rank == "auto" else delta_rank
+        self.conv_bias = conv_bias
+
+        self.input_proj = nn.Linear(self.model_dim, self.inner_dim * 2, bias=bias)
+        
+        self.conv = nn.Conv1d(
+            in_channels=self.inner_dim,
+            out_channels=self.inner_dim,
+            kernel_size=conv_kernel,
+            padding=conv_kernel - 1,
+            groups=self.inner_dim,
+            bias=conv_bias
+        )
+
+        self.ssm_param_proj = nn.Linear(
+            self.inner_dim,
+            self.delta_rank + self.state_dim * 2,
+            bias=False
+        )
+
+        self.delta_proj = nn.Linear(self.delta_rank, self.inner_dim, bias=True)
+
+        delta_std = self.delta_rank ** -0.5 * delta_scale
+        if delta_init == "constant":
+            nn.init.constant_(self.delta_proj.weight, delta_std)
+        elif delta_init == "random":
+            nn.init.uniform_(self.delta_proj.weight, -delta_std, delta_std)
+        else:
+            raise NotImplementedError
+    
+        delta = torch.exp(
+            torch.rand(self.inner_dim, device=device, dtype=torch.float32)
+            + math.log(delta_max) - math.log(delta_min)
+            + math.log(delta_min)
+        ).clamp(min=delta_init_floor)
+        inv_delta = delta + torch.log(-torch.expm1(-delta))
+        with torch.no_grad():
+            self.delta_proj.bias.copy_(inv_delta)
+
+        A = torch.arange(1, state_dim + 1, device=device, dtype=torch.float32)
+        A = A.unsqueeze(0).expand(self.inner_dim, state_dim).contiguous()
+        self.log_A = nn.Parameter(torch.log(A))
+
+        self.D = nn.Parameter(torch.ones(self.inner_dim, device=device))
+
+        self.out_proj = nn.Linear(self.inner_dim, model_dim, bias=bias)
+
+        self._ssm_hiddens = None
+        self._conv_context = None
+
+    def forward(self, hidden_states: torch.Tensor, lengths: torch.Tensor | None = None):
+        """
+        Args: (batch_size, seq_len, model_dim)
+        Returns: (batch_size, seq_len, model_dim)
+        """
+
+        batch_size, seq_len, _ = hidden_states.shape
+        device = hidden_states.device
+        use_cache = lengths is not None
+
+        # (batch_size, inner_dim, seq_len)
+        hidden_states, gate_logis = torch.chunk(
+            self.input_proj(hidden_states).permute(0, 2, 1).contiguous(),
+            chunks=2,
+            dim=1
+        )
+
+        if use_cache:
+            # Cache conv hidden
+            cache_size = self.conv_kernel - 1
+            window_start = lengths - cache_size
+            offsets = torch.arange(cache_size, device=device)
+            indices = window_start.unsqueeze(1) + offsets.unsqueeze(0)
+            valid_mask = (indices >= 0)
+            clamped_indices = indices.clamp(min=0)
+            clamped_indices = clamped_indices.unsqueeze(1).expand(-1, self.inner_dim, -1)
+            conv_context = torch.gather(
+                hidden_states,
+                dim=2,
+                index=clamped_indices
+            )
+            valid_mask = valid_mask.unsqueeze(1).expand(-1, self.inner_dim, -1)
+            conv_context *= valid_mask
+            self._conv_context = conv_context.detach()
+
+        hidden_states = self.conv(hidden_states)[..., :seq_len]
+        hidden_states = F.silu(hidden_states)
+        
+        ssm_params = self.ssm_param_proj(
+            hidden_states.permute(0, 2, 1)
+                .contiguous()
+                .view(batch_size * seq_len, self.inner_dim)
+        )
+
+        # delta: (batch_size * seq_len, delta_rank)
+        # B, C: (batch_size * seq_len, state_dim)
+        delta, B, C = torch.split(
+            ssm_params,
+            [self.delta_rank, self.state_dim, self.state_dim],
+            dim=-1
+        )
+
+        # (inner_dim, batch_size * seq_len)
+        delta = self.delta_proj.weight @ delta.t()
+
+        # (batch_size, inner_dim, seq_len)
+        delta = delta.view(self.inner_dim, batch_size, seq_len).permute(1, 0, 2).contiguous()
+
+        # (batch_size, state_dim, seq_len)
+        B = B.view(batch_size, seq_len, self.state_dim).permute(0, 2, 1).contiguous()
+        C = C.view(batch_size, seq_len, self.state_dim).permute(0, 2, 1).contiguous()
+
+        # (inner_dim, state_dim)
+        A = -torch.exp(self.log_A)
+
+        # out: (batch_size, inner_dim, seq_len)
+        # h: (batch_size, inner_dim, state_dim)
+        scan_outputs = SelectiveScanFn.apply(
+            hidden_states, 
+            A, B, C, self.D, 
+            delta, self.delta_proj.bias, 
+            lengths is not None
+        );
+
+        if use_cache:
+            hidden_states = scan_outputs[0]
+            self._ssm_hiddens = scan_outputs[1]
+        else:
+            hidden_states = scan_outputs
+        
+        # (batch_size, seq_len, inner_dim)
+        hidden_states = hidden_states.permute(0, 2, 1).contiguous()
+        hidden_states *= F.silu(gate_logis)
+
+        # (batch_size, seq_len, model_dim)
+        hidden_states = self.out_proj(hidden_states)
+        
+        return hidden_states
+
+    def step(self, hidden_states: torch.Tensor):
+        """
+        Args: (batch_size, model_dim)
+        Returns: (batch_size, model_dim)
+        """
+        
+        # (batch_size, inner_dim)
+        hidden_states, gate_logits = torch.chunk(
+            self.input_proj(hidden_states),
+            chunks=2,
+            dim=-1
+        )
+
+        # (batch_size, inner_dim, conv_kernel)
+        conv_context = torch.cat([self._conv_context, hidden_states.unsqueeze(-1)], dim=-1)
+        self._conv_context = conv_context[:, :, 1:].detach()
+        
+        # (inner_dim, conv_kernel)
+        conv_weight = self.conv.weight.squeeze(1)
+
+        # (batch_size, inner_dim)
+        hidden_states = (conv_context * conv_weight.unsqueeze(0)).sum(dim=-1)
+        if self.conv_bias:
+            hidden_states += self.conv.bias
+        hidden_states = F.silu(hidden_states)
+
+        # delta: (batch_size, delta_rank)
+        # B, C: (batch_size, state_dim)
+        delta, B, C = torch.split(
+            self.ssm_param_proj(hidden_states),
+            [self.delta_rank, self.state_dim, self.state_dim],
+            dim=-1
+        )
+
+        # (batch_size, inner_dim)
+        delta = self.delta_proj.weight @ delta.t()
+        delta = delta.t().contiguous()
+
+        # (inner_dim, state_dim)
+        A = -torch.exp(self.log_A)
+
+        # (batch_size, inner_dim), (batch_size, inner_dim, state_dim)
+        hidden_states, ssm_hiddens = SelectiveUpdateFn.apply(
+            hidden_states,
+            A, B, C, self.D,
+            delta, self.delta_proj.bias,
+            self._ssm_hiddens
+        )
+
+        self._ssm_hiddens = ssm_hiddens.detach()
+
+        hidden_states = hidden_states * F.silu(gate_logits)
+        hidden_states = self.out_proj(hidden_states)
+
+        return hidden_states
