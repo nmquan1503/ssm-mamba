0e5ae4ffb7741196161c71b226aba0b806c1a4bc
nmquan1503 <minhquan15032005@gmail.com>
2026-03-08 16:31:21 +0700

    feat: add optional h_init and always return h_last in python selective_scan interface

--- ops/selective_scan_interface.py
+++ ops/selective_scan_interface.py
@@ -14,9 +14,9 @@ class SelectiveScanFn(torch.autograd.Function):
         C: torch.Tensor, 
         D: torch.Tensor, 
         delta: torch.Tensor, 
-        delta_bias: torch.Tensor, 
+        delta_bias: torch.Tensor,
+        h_init: torch.Tensor | None = None,
         length: torch.Tensor | None = None,
-        return_last_hidden: bool = False
     ) -> Tuple[torch.Tensor, torch.Tensor] | torch.Tensor:
         """
         Args:
@@ -27,49 +27,51 @@ class SelectiveScanFn(torch.autograd.Function):
             D: (num_channels,)
             delta: (batch_size, num_channels, seq_len)
             delta_bias: (num_channels,)
+            h_init: (batch_size, num_channels, state_dim)
             length: (batch_size,)
         
         Returns:
-            If return_last_hidden is True:
-                out: (batch_size, num_channels, seq_len)
-                h: (batch_size, num_channels, state_dim)
-            
-            If return_last_hidden is False:
-                out: (batch_size, num_channels, seq_len)
+            out: (batch_size, num_channels, seq_len)
+            h_last: (batch_size, num_channels, state_dim)
+
                 
         """
-        u = ensure_contiguous(u);
-        A = ensure_contiguous(A)
-        B = ensure_contiguous(B)
-        C = ensure_contiguous(C)
-        D = ensure_contiguous(D)
-        delta = ensure_contiguous(delta)
-        delta_bias = ensure_contiguous(delta_bias)
-        if length is None:
-            batch_size, _, seq_len = u.shape
-            length = torch.full((batch_size,), seq_len, device=u.device, dtype=torch.long)
+        u, A, B, C, D, delta, delta_bias, h_init, length = map(
+            ensure_contiguous,
+            (u, A, B, C, D, delta, delta_bias, h_init, length)
+        )
 
-        out, h, last_h = selective_scan.forward(u, A, B, C, D, delta, delta_bias, length)
+        out, h = selective_scan.forward(
+            u, 
+            A, B, C, D, 
+            delta, delta_bias, 
+            h_init, length
+        )
 
-        if any(ctx.needs_input_grad[:7]):
-            ctx.save_for_backward(u, A, B, C, D, delta, delta_bias, h)
+        if any(ctx.needs_input_grad):
+            ctx.save_for_backward(
+                u, 
+                A, B, C, D, delta, delta_bias, 
+                h, h_init, length
+            )
         
-        if return_last_hidden:
-            return out, last_h
-        
-        return out
+        return out, h[..., 1::2]
 
     @staticmethod
-    def backward(ctx, dout):
+    def backward(ctx, dout, dh_last):
         if not hasattr(ctx, "saved_tensors") or len(ctx.saved_tensors) == 0:
             return (None,) * 8
         
         dout = ensure_contiguous(dout)
-        u, A, B, C, D, delta, delta_bias, h = ctx.saved_tensors
-        du, dA, dB, dC, dD, ddelta,ddelta_bias = selective_scan.backward(
+        dh_last = ensure_contiguous(dh_last)
+
+        u, A, B, C, D, delta, delta_bias, h, h_init, length = ctx.saved_tensors
+        du, dA, dB, dC, dD, ddelta, ddelta_bias, dh_init = selective_scan.backward(
             u, 
             A, B, C, D, 
             delta, delta_bias, 
-            h, dout
+            h, h_init, 
+            dout, dh_last, length
         )
-        return du, dA, dB, dC, dD, ddelta, ddelta_bias, None, None
+
+        return du, dA, dB, dC, dD, ddelta, ddelta_bias, dh_init, None
