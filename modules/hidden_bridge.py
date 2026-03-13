8612f4460a07ff2ec23f5cf2ed9653047753d997
nmquan1503 <minhquan15032005@gmail.com>
2026-03-14 00:15:49 +0700

    refactor: replace low-rank hidden bridge with custom gating activation

--- modules/hidden_bridge.py
+++ modules/hidden_bridge.py
@@ -3,21 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class HiddenBridge(nn.Module):
-    def __init__(self, channels: int, states: int):
+    def __init__(self, channels: int):
         super().__init__()
-        self.channels = channels
-        self.states = states
-        self.fc1 = nn.Linear(channels * states, channels)
-        self.fc2 = nn.Linear(channels, channels * states)
+
+        self.l1 = nn.Linear(channels, channels)
+        self.l2 = nn.Linear(channels, channels)
+        self.l3 = nn.Linear(channels, channels)
+
+    def _linear(self, hiddens: torch.Tensor, layer: nn.Linear):
+        hiddens = hiddens.transpose(1, 2)
+        hiddens = layer(hiddens)
+        hiddens = hiddens.transpose(1, 2)
+        return hiddens
+
+    def _active(self, hiddens: torch.Tensor):
+        s1 = F.relu(hiddens[:, :, 0]) 
+        mask = (s1 > 0).unsqueeze(-1) 
+        return hiddens * mask
     
     def forward(self, hiddens: torch.Tensor):
         """
         Args: (batch_size, num_channels, state_dim)
         Returns: (batch_size, num_channels, state_dim)
         """
-        batch_size = hiddens.size(0)
-        hiddens = hiddens.reshape(batch_size, self.channels * self.states)
-        hiddens = F.silu(self.fc1(hiddens))
-        hiddens = self.fc2(hiddens)
-        hiddens = hiddens.reshape(batch_size, self.channels, self.states)
+        hiddens = self._linear(hiddens, self.l1)
+        hiddens = self._active(hiddens)
+        hiddens = self._linear(hiddens, self.l2)
+        hiddens = self._active(hiddens)
+        hiddens = self._linear(hiddens, self.l3)
         return hiddens
